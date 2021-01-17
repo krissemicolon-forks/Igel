424c25b9dbb2bcd64ab75705a16a8b6febcba425
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-01-17 09:32:17 +0100

    added conffile for vhdl embeds

---
+++ include/igel.vhdl
@@ -0,0 +1,7 @@
+package igel_include is
+    shared variable IGEL_VERSION_MAJOR = 0;
+    shared variable IGEL_VERSION_MINOR = 1;
+    shared variable IGEL_VERSION_PATCH = 2;
+    shared variable IGEL_VERSION_EXTRA = "-dev"
+    shared variable IGEL_VERSION "0.1.2-dev"
+end package igel_include;
